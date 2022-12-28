85c22a25a2448fd86aed4f53b7e9b548091420b4
GUchechi <godswilluchendu7@gmail.com>
2022-12-28 20:24:51 +0100

    added navBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,19 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 
 const NavBar = () => {
+  const [nav, setNav] = useState(false);
+
+  const handleNav = () => {
+    setNav(!nav);
+  };
   return (
     <div className="w-full h-[90px] bg-black">
       <div className="max-w-[1240px] mx-auto px-4 flex items-center justify-between h-full">
         <div>
           <h1 className="text-[#00d8ff]">DEFI</h1>
         </div>
-        <div>
+        <div className="hidden md:flex">
           <ul className="flex text-white items-center">
             <li>Platform</li>
             <li>Developers</li>
             <li>Community</li>
             <li>About</li>
-            <button>Use Defi</button>
+            <button className="ml-4">Use Defi</button>
+          </ul>
+        </div>
+        {/* Hamburger Menu */}
+        <div className="block md:hidden" onClick={handleNav}>
+          {nav ? (
+            <AiOutlineClose size={30} className="text-white" />
+          ) : (
+            <AiOutlineMenu size={30} className="text-white" />
+          )}
+        </div>
+        {/* Mobile Menu */}
+        <div className={nav? 'w-full bg-black text-white absolute top-[90px] left-0 flex justify-center text-center' : 'absolute left-[-100%]'}>
+          <ul>
+            <li className="text-2xl">Platform</li>
+            <li className="text-2xl">Developers</li>
+            <li className="text-2xl">Community</li>
+            <li className="text-2xl">About</li>
+            <button className="m-8">Use Defi</button>
           </ul>
         </div>
       </div>
